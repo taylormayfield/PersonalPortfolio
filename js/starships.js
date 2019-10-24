03802e7265ad8de1a75759e113db2dae920daa89
Taylor Mayfield <taylormayfield@taylors-air-2.ad.uvu.edu>
2019-10-24 12:29:46 -0600

    starships

--- js/starships.js
+++ js/starships.js
@@ -7,14 +7,24 @@ starships.forEach(ship => {
     let shipName = document.createElement('h3')
     let shipPic = document.createElement('img')
 
-    shipName.textContent = ship.name
-
-    let shipNum = getShipNumber(ship.url)
-
-    shipPic.src = 'https://starwars-visualguide.com/assets/img/starships/${shipNum}.jpg`
-    
-
     shipDiv.appendChild(shipName)
     shipDiv.appendChild(shipPic)
     contentArea.appendChild(shipDiv)
+
+    let shipNum = getShipNumber(ship.url)
+
+    shipName.textContent = ship.name
+    shipPic.src = 'https://starwars-visualguide.com/assets/img/starships/${shipNum}.jpg`
+
+    mainArea.appendChild(shipDiv)
 })
+
+function getShipNumber(charURL) {
+    let end = shipURL.lastIndexOf('/')
+    let shipID = shipURL.substring(end - 2, end)
+    if (shipID.indexOf('/') !== -1) {
+        return shipID.slice(1, 2)
+    } else {
+        return shipID
+    }
+} 
