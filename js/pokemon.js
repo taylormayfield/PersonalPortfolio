827f9082890417d63943a2bb81ed6bf07869bc8e
Taylor Mayfield <taylormayfield@taylors-air-2.ad.uvu.edu>
2019-10-29 11:14:40 -0600

    adding things

---
+++ js/pokemon.js
@@ -0,0 +1,30 @@
+
+const allPokemon = (async () => {
+const response = await fetch('https://pokeapi.co/api/v2/pokemon/')
+const myJson = await response.json()
+console.log(JSON.stringify(myJson))
+})
+
+
+console.log(allPokemon().then)
+
+
+/* people.forEach(function(person) {
+    let personDiv = document.createElement('div')
+    let name = document.createElement('h1')
+    let gender = document.createElement('p')
+    let pic = document.createElement('img')
+
+    personDiv.appendChild(name)
+    personDiv.appendChild(gender)
+    personDiv.appendChild(pic)
+
+    let charNum = getCharNumber(person.url)
+
+    name.textContent = person.name
+    gender.textContent = gender.name
+    pic.src = `https://starwars-visualguide.com/assets/img/characters/${charNum}.jpg`
+
+    mainArea.appendChild(personDiv)
+})*/
+
