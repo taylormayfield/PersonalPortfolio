f78f463973df068fefc6d5580a1763236151e9ad
Taylor Mayfield <taylormayfield@Taylors-MacBook-Air-2.local>
2019-10-30 13:00:13 -0600

    trying to get planets up and running

--- js/planets.js
+++ js/planets.js
@@ -15,6 +15,8 @@ planets.forEach(planet => {
 
     planetName.textContent = planet.name
     planetPic.src = `https://starwars-visualguide.com/assets/img/planets/${planetNum}.jpg`
+
+    
 })
 
 function getCharNumber(charURL) {
@@ -26,31 +28,3 @@ function getCharNumber(charURL) {
         return charID
     }
 } 
-
-/*let mainHeader = document.querySelector('header')
-let mainArea = document.querySelector('main')
-
-planets.forEach(planet => {
-    let planetDiv = document.createElement('div')
-    let planetName = document.createElement('h3')
-    let planetPic = document.createElement('img')
-
-    planetDiv.appendChild(planetName)
-    planetDiv.appendChild(planetPic)
-    contentArea.appendChild(planetDiv)
-
-    let planetNum = getPlanetNumber(planet.url)
-
-    planetName.textContent = planet.name
-    planetPic.src = `https://starwars-visualguide.com/assets/img/planets/${planetNum}.jpg`
-})
-
-function getPlanetNumber(planetURL) {
-    let end = planetURL.lastIndexOf('/')
-    let planetID = planetURL.substring(end - 2, end)
-    if (planetID.indexOf('/') !== -1) {
-        return planetID.slice(1, 2)
-    } else {
-        return planetID
-    }
-} */
