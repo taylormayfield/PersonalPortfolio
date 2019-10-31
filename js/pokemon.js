0d8fe114946248d00c837795ebb47eb3999f38c6
Taylor Mayfield <taylormayfield@taylors-air-2.ad.uvu.edu>
2019-10-31 10:29:14 -0600

    pokedata

--- js/pokemon.js
+++ js/pokemon.js
@@ -2,7 +2,7 @@
 async function getAPIData(url) {
     try {
         const response = await fetch(url)
-        const data =await response.json()
+        const data = await response.json()
         return data
     } catch (error) {
         console.error(error)
@@ -10,7 +10,16 @@ async function getAPIData(url) {
 }
 
 const theData = getAPIData('https://pokeapi.co/api/v2/pokemon/')
+    .then(data => {
+        for (const pokemon of data.results) {
+            getAPIData(pokemon.url)
+                .then(pokedata => {
+                    console.log(pokedata)
+                })
+        }
+    })
 
+console.log(theData)
 
 
 
@@ -19,11 +28,11 @@ let mainArea = document.querySelector('main')
 function populateDOM(pokeArray) {
     pokeArray.forEach(pokemon => {
         console.log(pokemon)
-    let pokeDiv = document.createElement('div')
-    let name = document.createElement('h3')
-    let pic = document.createElement('img')
+        let pokeDiv = document.createElement('div')
+        let name = document.createElement('h3')
+        let pic = document.createElement('img')
 
-    pokeDiv.setAttribute('class', 'charDivs')
-    pic.setAttribute('class', 'picDiv')
+        pokeDiv.setAttribute('class', 'charDivs')
+        pic.setAttribute('class', 'picDiv')
     })
 }
