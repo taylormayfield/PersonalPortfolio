1a18a0476e4529f580836bd0fdaec5830e3c3231
Taylor Mayfield <taylormayfield@taylors-air-2.ad.uvu.edu>
2019-10-31 11:00:01 -0600

    adding things

--- js/pokemon.js
+++ js/pokemon.js
@@ -25,14 +25,29 @@ console.log(theData)
 
 let mainArea = document.querySelector('main')
 
-function populateDOM(pokeArray) {
-    pokeArray.forEach(pokemon => {
-        console.log(pokemon)
+function populateDOM(single_pokemon) {
         let pokeDiv = document.createElement('div')
         let name = document.createElement('h3')
         let pic = document.createElement('img')
 
         pokeDiv.setAttribute('class', 'charDivs')
-        pic.setAttribute('class', 'picDiv')
-    })
-}
+        pic.setAttribute('class', 'picDivs')
+
+        let pokeNum = getPokeNumber(single_pokemon.id)
+
+        name.texContent = single_pokemon.name
+
+        pic.src = `..images/${pokeNum}.png`
+
+        pokeDiv.appendChild(name)
+        pokeDiv.appendChild(pic)
+
+        mainArea.appendChild(pokeDiv)
+    }
+
+function getPokeNumber(id) {
+            if (id < 10) return `00${id}`
+            if (id > 9 && id < 100) {
+                return `0${id}`
+            } else return id
+        }
