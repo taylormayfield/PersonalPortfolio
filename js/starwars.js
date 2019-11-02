eb9d752ddcb23751abb73164c5dd08ba03ba992f
Taylor Mayfield <taylormayfield@Taylors-MacBook-Air-2.local>
2019-11-01 18:00:28 -0600

    rick and morty

--- js/starwars.js
+++ js/starwars.js
@@ -5,58 +5,6 @@ console.log('Hey, I am JavaScript on your page!')
 
 console.log('films')
 
-let mainHeader = document.querySelector('header')
-let mainArea = document.querySelector('main')
-
-
-/* films.forEach(function (film) {
-    let filmDiv = document.createElement('div')
-    let filmTitle = document.createElement('h1')
-    let filmCrawl = document.createElement('p')
-
-    filmDiv.appendChild(filmTitle)
-    filmDiv.appendChild(filmCrawl)
-
-    filmTitle.textContent = film.title
-    filmCrawl.innerText = film.opening_crawl
-
-    mainArea.appendChild(filmDiv)
-}); */
-
-
-
-
-const maleCharacters = people.filter(person => person.gender === 'male')
-const femaleCharacters = people.filter(person => person.gender === 'female')
-const otherCharacters = people.filter(person => person.gender !== 'female' && person.gender !== 'male')
-const allDivs = Array.from(mainArea.querySelectorAll('div'))
-console.log(maleCharacters)
-console.log(femaleCharacters)
-
-let maleButton = document.createElement('button')
-maleButton.textContent = "Male Characters"
-maleButton.addEventListener('click', () => {
-    maleCharacters.forEach(elt => {
-        let matchedDiv = allDivs.filter(element => {
-            return element.firstChild.textContent === elt.name
-        })
-        matchedDiv[0].setAttribute("style", "display: none;")
-    })
-})
-
-let femaleButton = document.createElement('button')
-femaleButton.textContent = "Female Character"
-femaleButton.addEventListener('click', event => {
-    femaleCharacters.forEach(elt => {
-        let matchedDiv = allDivs.filter(element => {
-            return element.firstChild.textContent === elt.name
-        })
-        matchedDiv[0].setAttribute("style", "display: revert;")
-    })
-})
-    mainHeader.appendChild(maleButton)
-    mainHeader.appendChild(femaleButton)
-
 people.forEach(function(person) {
     let personDiv = document.createElement('div')
     let name = document.createElement('h1')
@@ -85,3 +33,57 @@ function getCharNumber(charURL) {
         return charID
     }
 }
+
+let mainHeader = document.querySelector('header')
+let mainArea = document.querySelector('main')
+
+const allDivs =Array.from(document.querySelectorAll('div'))
+
+const mainHeader = document.querySelector('header')
+let maleButton = document.createElement('button')
+maleButton.textContent = 'Male Characters'
+
+/*const maleCharacters = people.filter(person => person.gender === 'male')
+const femaleCharacters = people.filter(person => person.gender === 'female')
+const otherCharacters = people.filter(person => person.gender !== 'female' && person.gender !== 'male')
+const allDivs = Array.from(mainArea.querySelectorAll('div'))
+console.log(maleCharacters)
+console.log(femaleCharacters)
+
+let maleButton = document.createElement('button')
+maleButton.textContent = "Male Characters"
+maleButton.addEventListener('click', () => {
+    maleCharacters.forEach(elt => {
+        let matchedDiv = allDivs.filter(element => {
+            return element.firstChild.textContent === elt.name
+        })
+        matchedDiv[0].setAttribute("style", "display: none;")
+    })
+})
+
+let femaleButton = document.createElement('button')
+femaleButton.textContent = "Female Character"
+femaleButton.addEventListener('click', event => {
+    femaleCharacters.forEach(elt => {
+        let matchedDiv = allDivs.filter(element => {
+            return element.firstChild.textContent === elt.name
+        })
+        matchedDiv[0].setAttribute("style", "display: revert;")
+    })
+})
+    mainHeader.appendChild(maleButton)
+    mainHeader.appendChild(femaleButton)*/
+
+    /* films.forEach(function (film) {
+    let filmDiv = document.createElement('div')
+    let filmTitle = document.createElement('h1')
+    let filmCrawl = document.createElement('p')
+
+    filmDiv.appendChild(filmTitle)
+    filmDiv.appendChild(filmCrawl)
+
+    filmTitle.textContent = film.title
+    filmCrawl.innerText = film.opening_crawl
+
+    mainArea.appendChild(filmDiv)
+}); */
