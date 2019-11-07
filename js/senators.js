0b77bdb6a5c05a49c9aad2774a572fac6a532345
Taylor Mayfield <taylormayfield@taylors-air-2.ad.uvu.edu>
2019-11-07 11:14:27 -0700

    stuff

--- js/senators.js
+++ js/senators.js
@@ -10,6 +10,7 @@ async function getAPIData(url) {
 let allSenators = []
 const theData = getAPIData('senators.json').then(data => {
     allSenators = data.results[0].members
+    populateDOM(allSenators)
 })
 
 const republicans = allSenators.filter(senator => senator.party === 'R')
@@ -18,5 +19,48 @@ const democrates = allSenators.filter(senator => senator.party === 'D')
 const container = document.querySelector('.container')
 
 function populateDOM(senatorArray) {
-    
+    senatorArray.forEach(senator => {
+    let card = document.createElement('div')
+    card.setAttribute('class', 'card')
+    let cardImage = document.createElement('div')
+    cardImage.setAttribute('class', 'card-image')
+    let cardFigure = document.createElement('figure')
+    //cardFigure.setAttribute('class', 'image is -4by3')
+    let figureImage = document.createElement('img')
+    figureImage.src = `https://www.congress.gov/img/member/${senator.id.toLowerCase()}_200.jpg`
+    figureImage.alt = "Placeholder Image"
+
+    cardFigure.appendChild(figureImage)
+    cardImage.appendChild(cardImage)
+    card.appendChild(cardImage)
+    card.appendChild(populateCardContent(senator))
+    container.appendChild(card)
+})
+}
+
+function populateCardContent(senator) {
+    let cardContent = document.createElement('div')
+    cardContent.setAttribute('class', 'card-content')
+    let media = document.createElement('div')
+    media.setAttribute('class', 'media')
+    let mediaLeft =document.createElement('div')
+    mediaLeft.setAttribute('class', 'media-left')
+    let figure = document.createElement('figure')
+    figure.setAttribute('class', 'image is-48x48')
+    let figureImage = document.createElement('img')
+    figureImage.src = ""
+    let mediaContent = document.createElement('div')
+    mediaContent.setAttribute('class', 'media-content')
+    let titleP = document.createElement('p')
+    titleP.setAttribute('class', 'title is-4')
+    let subtitleP =documemt.createElement('p')
+    subtitleP.setAttribute('class', 'subtitle is -6')
+
+    mediaContent.appendChild(titleP)
+    mediaContent.appendChild(subtitleP)
+    figure.appendChild(figureImage)
+    mediaLeft.appendChild(figure)
+    media.appendChild(mediaLeft)
+    media.appendChild(mediaContent)
+    cardContent.appendChild()
 }
