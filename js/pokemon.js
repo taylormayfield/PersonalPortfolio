a5071dea4975a4044afe3022d52494fc15d7b511
Taylor Mayfield <taylormayfield@taylors-air-2.ad.uvu.edu>
2019-11-14 11:15:12 -0700

    changing up pokecode

--- js/pokemon.js
+++ js/pokemon.js
@@ -1,3 +1,14 @@
+class Pokemon {
+    constructor(id, name, stats) {
+        this.id = id
+        this.name = name
+        this.stats = stats
+    }
+}
+
+const Thoremon = new Pokemon(900, 'Thoremon', 130)
+
+populateDOM(Thoremon)
 
 async function getAPIData(url) {
     try {
@@ -12,10 +23,9 @@ async function getAPIData(url) {
 const theData = getAPIData('https://pokeapi.co/api/v2/pokemon/')
     .then(data => {
         for (const pokemon of data.results) {
-            getAPIData(pokemon.url)
-                .then(pokedata => {
-                    populateDOM(pokedata)
-                })
+            getAPIData(pokemon.url).then(pokedata => {
+                populateDOM(pokedata)
+            })
         }
     })
 
@@ -26,51 +36,63 @@ console.log(theData)
 let mainArea = document.querySelector('main')
 
 function populateDOM(single_pokemon) {
-        let pokeScene = document.createElement('div')
-        let pokeCard = document.createElement('div')
-        let pokeFront = document.createElement('div')
-        let pokeBack = document.createElement('div')
-        let name = document.createElement('p')
-        let pic = document.createElement('img')
+    let pokeScene = document.createElement('div')
+    let pokeCard = document.createElement('div')
+    let pokeFront = document.createElement('div')
+    let pokeBack = document.createElement('div')
 
-       // fillCardFront(pokeFront, single_pokemon)
-        fillCardBack(pokeBack, single_pokemon)
+    fillCardFront(pokeFront, single_pokemon)
+    fillCardBack(pokeBack, single_pokemon)
 
-        pokeScene.setAttribute('class', 'scene')
-        pokeCard.setAttribute('class', 'card')
-        pokeFront.setAttribute('class', 'charDivs card__face card__face--front')
-        pokeBack.setAttribute('class', 'card__face card__face--back')
-        pic.setAttribute('class', 'picDivs')
+    pokeScene.setAttribute('class', 'scene')
+    pokeCard.setAttribute('class', 'card')
+    pokeCard.setAttribute(pokeFront)
+    pokeCard.setAttribute(pokeBack)
+    pokeScene.setAttribute(pokeCard)
 
-        let pokeNum = getPokeNumber(single_pokemon.id)
-        pokeFront.appendChild(name)
-        name.texContent = `${single_pokemon.name} height: ${single_pokemon.height}`
+    let pokeNum = getPokeNumber(single_pokemon.id)
+    pokeFront.appendChild(name)
+    name.texContent = `${single_pokemon.name} height: ${single_pokemon.height}`
 
-        pic.src = `../images/${pokeNum}.png`
-        pokeFront.appendChild(pic)
-        pokeFront.appendChild(name)
+    pokeCard.appendChild(pokeFront)
+    pokeCard.appendChild(pokeBack)
+    pokeScene.appendChild(pokeCard)
 
-        pokeCard.appendChild(pokeFront)
-        pokeCard.appendChild(pokeBack)
-        pokeScene.appendChild(pokeCard)
+    mainArea.appendChild(pokeScene)
 
-        mainArea.appendChild(pokeScene)
+    pokeCard.addEventListener('click', function () {
+        pokeCard.classList.toggle('is-flipped');
+    });
+}
 
-        pokeCard.addEventListener('click', function() {
-            pokeCard.classList.toggle('is-flipped');
-        });
-    }
+function fillCardFront(pokeFront, data) {
+    pokeFront.setAttribute('class', 'card_face card_face--front')
+    let name = document.createElement('p')
+    let pic = document.createElement('img')
+    pic.setAttribute('class', 'picDivs')
+    let pokeNum = getPokeNumber(data.id)
+    pokeFront.appendChild(name)
+    pic.src = `../images/${pokeNum}.png`
 
-    function fillCardBack(pokeBack, data) {
-        let pokeOrder = document.createElement('p')
-        pokeOrder.textContent = data.order
-       pokeBack.appendChild(pokeOrder)
-    }
+    pokeFront.appendChild(pic)
+    pokeFront.appendChild(name)
+}
+
+function fillCardBack(pokeBack, data) {
+    pokeBack.setAttribute('class', 'card_face card_face--back')
+    let pokeOrder = document.createElement('p')
+    let pokeHP = document.createElement('h5')
+    pokeOrder.textContent = `#${data.id} ${data.name[0].toUpperCase()} ${data.name.slice(1)}`
+    pokeHP.textContent = data.stats[0].base_stat
+    pokeBack.appendChild(pokeOrder)
+    pokeBack.appendChild(pokeHP)
+}
 
 
 function getPokeNumber(id) {
-            if (id < 10) return `00${id}`
-            if (id > 9 && id < 100) {
-                return `0${id}`
-            } else return id
-        }
+    if (id < 10) return `00${id}`
+    if (id > 9 && id < 100) {
+        return `0${id}`
+    } else return id
+}
+
