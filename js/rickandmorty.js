0c8d8f8cf5ba2543d026438cc238c5147dd661da
taylormayfield <taylormichellemayfield@gmail.com>
2019-11-22 10:03:47 -0700

    rick and morty

--- js/rickandmorty.js
+++ js/rickandmorty.js
@@ -1,13 +1,50 @@
 async function getData() 
         {
-            //await the response of the fetch call
            let response = await fetch('https://rickandmortyapi.com/api/character/');
-            //proceed once the first promise is resolved.
            let data = await response.json()
-            //proceed only when the second promise is resolved
             return data;
         }
-//call getData function
+
 getData()
-.then(data => console.log(data));//log the data
-  //https://rickandmortyapi.com/api/character/ get all char
+.then(data => console.log(data));
+
+
+function showCharArray(arrayOfPeople){
+  arrayOfPeople.forEach(person => {
+      let personDiv = document.createElement('div')
+      let name = document.createElement('h1')
+      let gender = document.createElement('p')
+      let pic = document.createElement('img')
+  
+      personDiv.setAttribute('class', 'charDivs')
+      pic.setAttribute('class', 'picDivs')
+  
+  
+      let charNum = getCharNumber(person.url)
+  
+      name.textContent = person.name
+      gender.textContent = `${person.gender}`
+      pic.src = `"https://rickandmortyapi.com/api/character/avatar/${charNum}.jpeg"`
+
+      console.log(charNum)
+  
+      personDiv.appendChild(name)
+      personDiv.appendChild(gender)
+      personDiv.appendChild(pic)
+  
+      mainArea.appendChild(personDiv)
+  })
+  }
+  
+  
+  function getCharNumber(charURL) {
+      let end = charURL.lastIndexOf('/')
+      let charID = charURL.substring(end - 2, end)
+      if (charID.indexOf('/') !== -1) {
+          return charID.slice(1, 2)
+      } else {
+          return charID
+      }
+  }
+
+  //images "https://rickandmortyapi.com/api/character/avatar/.jpeg"
